1eeb7853bed8b9480fe398e78c42c2cd9c9b8e21
scheals <piecoo@gmail.com>
2022-06-08 02:05:41 +0200

    Add default user_id value

--- Ruby/lib/authentication_service.rb
+++ Ruby/lib/authentication_service.rb
@@ -7,7 +7,7 @@ class AuthenticationService
   # def authenticated?(role, user_id)
   #
 
-  def authenticated?(role = :FIX_ME, user_id)
+  def authenticated?(role = :FIX_ME, user_id = 'FIX_ME')
     user_id = role if user_id.is_a?(String)
     user_id == 12_345
   end
